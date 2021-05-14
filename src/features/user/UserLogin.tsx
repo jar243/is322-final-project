603db63d0d85edcccb97e8e79f51af340eb58881
fake name <bruh@fam.com>
2021-05-13 21:33:30 -0400

    added disclaimer to login

--- src/features/user/UserLogin.tsx
+++ src/features/user/UserLogin.tsx
@@ -34,47 +34,56 @@ export function UserLogin() {
   const dispatch = useAppDispatch()
 
   return (
-    <Card style={cardStyle}>
-      <H2 style={mb(20)}>Welcome back!</H2>
-      {status === "failed" && (
-        <Callout style={mb(20)}>Invalid username or password</Callout>
-      )}
-      <FormGroup disabled={status === "pending"}>
-        <Label>
-          Username
-          <InputGroup
-            placeholder="john"
+    <div style={cardStyle}>
+      <Card style={mb(40)}>
+        <H2 style={mb(20)}>Welcome back!</H2>
+        {status === "failed" && (
+          <Callout style={mb(20)}>Invalid username or password</Callout>
+        )}
+        <FormGroup disabled={status === "pending"}>
+          <Label>
+            Username
+            <InputGroup
+              placeholder="john"
+              disabled={status === "pending"}
+              onChange={(e) => {
+                dispatch(setUsername(e.target.value))
+              }}
+            />
+          </Label>
+          <Label style={mb(20)}>
+            Password
+            <InputGroup
+              placeholder="password"
+              type="password"
+              disabled={status === "pending"}
+              onChange={(e) => {
+                dispatch(setPassword(e.target.value))
+              }}
+            />
+          </Label>
+          <Button
             disabled={status === "pending"}
-            onChange={(e) => {
-              dispatch(setUsername(e.target.value))
+            onClick={() => {
+              dispatch(
+                login({
+                  username,
+                  password,
+                })
+              )
             }}
-          />
-        </Label>
-        <Label style={mb(20)}>
-          Password
-          <InputGroup
-            placeholder="password"
-            type="password"
-            disabled={status === "pending"}
-            onChange={(e) => {
-              dispatch(setPassword(e.target.value))
-            }}
-          />
-        </Label>
-        <Button
-          disabled={status === "pending"}
-          onClick={() => {
-            dispatch(
-              login({
-                username,
-                password,
-              })
-            )
-          }}
-        >
-          Login
-        </Button>
-      </FormGroup>
-    </Card>
+          >
+            Login
+          </Button>
+        </FormGroup>
+      </Card>
+      <Callout>
+        Use the placeholder username and password values to login, or add your
+        own to the db.json in the root dir <br /> <br />
+        The login method I used with json-server gives the illusion of a normal
+        login system but is completely insecure. In a real app I would use
+        tokens, etc
+      </Callout>
+    </div>
   )
 }
